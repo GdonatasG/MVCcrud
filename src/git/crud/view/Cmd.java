13810e31aa8273e1a7a70f0d8e084c417084ba99
GdonatasG <zitkusd@gmail.com>
2018-10-14 14:14:47 +0300

    CommandLine showMeniu method.

---
+++ src/git/crud/view/Cmd.java
@@ -0,0 +1,13 @@
+package git.crud.view;
+
+public class Cmd {
+    public void showMeniu() {
+        System.out.println("Pasirinkite ka noresite daryti");
+        System.out.println("0 - baigti darba");
+        System.out.println("1 - nurodyti failo varda");
+        System.out.println("2 - sukurti faila");
+        System.out.println("3 - perskaityti faila");
+        System.out.println("4 - papildyti faila");
+        System.out.println("5 - istrinti faila");
+    }
+}
